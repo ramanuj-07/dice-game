2b72fd570f69c234c2f693702fffcdd9338d735f
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-05-21 10:59:06 +0530

    Update RollDice.jsx

--- dice-game/src/components/RollDice.jsx
+++ dice-game/src/components/RollDice.jsx
@@ -1,14 +1,28 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { useState } from 'react';
 
 export default function RollDice() {
+    const [currentDice, setCurrentDice] = useState(1);
+
+    const generateRandomNumber = (min, max) => {
+         return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function rollDice() {
+        const randomNumber = generateRandomNumber(1, 7);
+        setCurrentDice((prev) => randomNumber);
+
+    };
     return (
         <RollDiceInterface>
-            <img src="/images/dices.png" alt="Dice Image" />
+            <img onClick={rollDice}
+                src={`/images/dice_${currentDice}.png`} alt="Dice Image" /> 
             <p>Click on Dice to roll</p>
             <div className="result">
                 <button>Reset Score</button>
                 <button>Show Rules</button>
             </div>
+           
         </RollDiceInterface>
     )
 }
@@ -18,9 +32,12 @@ const RollDiceInterface = styled.div`
     display:flex;
     align-items:center;
     flex-direction:column;
+
     img{
-    height:300px;
-    width:300px;
+    height:200px;
+    width:200px;
+    margin-top: 50px;
+    cursor: pointer;
     }
     p{
     font-weight:500;
